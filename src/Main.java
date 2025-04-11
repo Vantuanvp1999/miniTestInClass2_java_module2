fda7fb4329ac995f537d452c651f091150ed7b5c
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-11 15:31:58 +0700

    fix clean code

--- src/Main.java
+++ src/Main.java
@@ -13,42 +13,53 @@ public class Main {
     for (Product p : products) {
         System.out.println(p);
     }
-    for (Product p : products) {
-        if(p.getName().equals("Ao")){
-            System.out.println(p);
-        }else{
-            System.out.println("Khong tim thay san pham");
-        }
-    }
-    for (Product p : products) {
+        findElement(products);
+        for (Product p : products) {
         if(p.getPrice()>1000000){
             System.out.println(p);
         }
     }
-    int a = products[0].getQuantity();
-    int b =-1;
-        for(int i=1;i<products.length;i++){
+        findMaxQuantity(products);
+
+        findSumFunction(products);
+
+        Product.storeName="Kho miền Nam";
+        System.out.println(products[1].getStoreName());
+    }
+
+    public static void findSumFunction(Product[] products) {
+        int sumProduct=0;
+        double sumPrice=0;
+        double sumIventoryValue=0;
+        for(int i = 0; i< products.length; i++){
+            sumProduct+= products[i].getQuantity();
+            sumPrice+= products[i].getPrice();
+            sumIventoryValue+= products[i].getTotalInventoryValue();
+        }
+        System.out.println(sumProduct);
+        System.out.println(sumPrice/5);
+        System.out.println(sumIventoryValue);
+    }
+
+    public static void findMaxQuantity(Product[] products) {
+        int a = products[0].getQuantity();
+        int b =-1;
+        for(int i = 1; i< products.length; i++){
             if(products[i].getQuantity()>a){
                 b=i;
             }
         }
 
         System.out.println(products[b]);
+    }
 
-        int sumProduct=0;
-        double sumPrice=0;
-        double sumIventoryValue=0;
-        for(int i=0;i<products.length;i++){
-            sumProduct+=products[i].getQuantity();
-            sumPrice+=products[i].getPrice();
-            sumIventoryValue+=products[i].getTotalInventoryValue();
+    public static void findElement(Product[] products) {
+        for (Product p : products) {
+            if(p.getName().equals("Ao")){
+                System.out.println(p);
+            }else{
+                System.out.println("Khong tim thay san pham");
+            }
         }
-        System.out.println(Product.totalProduct);
-        System.out.println(sumPrice/5);
-        System.out.println(sumIventoryValue);
-
-        Product.storeName="Kho miền Nam";
-        System.out.println(products[1].getStoreName());
-
     }
 }
